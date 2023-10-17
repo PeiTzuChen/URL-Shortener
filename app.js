8b0a9a75e467bd18f97b38b28d6c625219f440d8
peitzuchen <prettysna@hotmail.com>
2023-10-17 18:11:17 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('./public'))
+
 app.get('/',(req,res) => {
+res.redirect('/shortlink')
+
+})
+
+app.get('/shortlink',(req,res) => {
 
 
 })
