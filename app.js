fd1ed57c1321dab74debc916fa23b460f00d2aa5
peitzuchen <prettysna@hotmail.com>
2023-10-19 11:45:25 +0800

    build:template engine & add listening page

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { engine } = require('express-handlebars')
 
 app.use(express.static('public'))
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
 
 app.get('/',(req,res) => {
 res.redirect('/shortlink')
 
 })
 
-app.get('/shortlink',(req,res) => {
-res.sendFile(__dirname + '/public/main.html');
 
+
+app.get('/shortlink',(req,res) => {
+res.render('finished')
 
 })
+
+
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 } )
+
+
+
 
