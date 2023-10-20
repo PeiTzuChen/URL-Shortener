a782c3fe5985177202d31a711a7f348b864028d8
peitzuchen <prettysna@hotmail.com>
2023-10-20 11:39:05 +0800

    build: revise method of reading JSON and add shortened link to JSON

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const port = 3000
 const { engine } = require('express-handlebars')
 const fs = require('fs'); 
 
-
-
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
@@ -18,9 +16,18 @@ app.get('/',(req,res) => {
 res.render('index')
 })
 
-//讀取本地JSON檔（同步方式）
-let data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+
+//使用list變數裝資料，讀取本地JSON檔（同步方式）
+let list = [];
+  try {
+    list = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    console.log('list',list)
+  } catch (error) {
+    console.log('list沒東西')
+  }
+
 const generateRandom = require('./js_module/generateRandom.js');
+const BASE_URL = 'http://localhost:3000/'
 
 //收到資料
 app.post('/',(req,res) => {
@@ -31,16 +38,16 @@ if (!originURL.startsWith('http')) {
   res.render('err',{warning : 'Please paste correct form of link!'})
 }
 
-
-
-else
-
-
-
-res.render('index')
+else {  //建立物件儲存原網址與短網址
+  const shortenedURL = {
+    origin: originURL,
+    shortened: BASE_URL+generateRandom()
+  }
+  list.push(shortenedURL)
+  fs.writeFileSync('data.json', JSON.stringify(list, null, 2));
+  res.render('index')
+}
 })
-
-
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
